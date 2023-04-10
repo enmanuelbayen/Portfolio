a170be762cf4a7636465ec308fa20b1c48270496
enmanuelbayen <enmanuel0495@gmail.com>
2023-04-10 18:55:25 -0400

    Add JS features and some css style

--- JS/DOM.js
+++ JS/DOM.js
@@ -1,2 +1,23 @@
 console.log("***** Elementos del DOM.js *****");
 
+const mobileMenu = document.querySelector('.contenedor');
+const menuBttn = document.getElementById('menu-icon');
+const anchorList = document.querySelectorAll('.anchor-section');
+
+menuBttn.addEventListener('click', () => {
+    mobileMenu.style.display = 'block';
+})
+
+const remove = document.querySelector('.x-remove');
+
+remove.addEventListener('click', () => {
+    mobileMenu.style.display = 'none';
+})
+
+anchorList.forEach((anchorList) => {
+    anchorList.addEventListener('click', () => {
+        mobileMenu.style.display = 'none';
+    });
+});
+
+
