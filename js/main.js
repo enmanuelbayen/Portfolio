49c9e50524b2c07f922d3215599400e3f6c90460
enmanuelbayen <enmanuel0495@gmail.com>
2023-04-11 14:42:07 -0400

    Add js object and popUp setup fuction

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//mobile-menu
 const mobileMenu = document.querySelector('.contenedor');
 const menuBttn = document.getElementById('menu-icon');
 const anchorList = document.querySelectorAll('.anchor-section');
@@ -17,3 +18,87 @@ anchorList.forEach((anchorList) => {
     mobileMenu.style.display = 'none';
   });
 });
+// project.popUp
+const projectDeck = {
+  
+  projectNro1: {
+    img: '',
+    title: 'Profesional Art Printing Data',
+    descrip: 'A daily selection of privately personalized read',
+    tech1: 'HTML',
+    tech2: 'Bootstrap',
+    tech3: 'Ruby',
+    liveBttn: '',
+    sourceBttn: '',
+  }, 
+
+  projectNro2: {
+    img: '',
+    title: 'Profesional Art Printing Data',
+    descrip: 'A daily selection of privately personalized read',
+    tech1: 'HTML',
+    tech2: 'Bootstrap',
+    tech3: 'Ruby',
+    liveBttn: '',
+    sourceBttn: '',
+  }, 
+
+  projectNro3: {
+    img: '',
+    title: 'Profesional Art Printing Data',
+    descrip: 'A daily selection of privately personalized read',
+    tech1: 'HTML',
+    tech2: 'Bootstrap',
+    tech3: 'Ruby',
+    liveBttn: '',
+    sourceBttn: '',
+  }, 
+
+  projectNro4: {
+    img: '',
+    title: 'Profesional Art Printing Data',
+    descrip: 'A daily selection of privately personalized read',
+    tech1: 'HTML',
+    tech2: 'Bootstrap',
+    tech3: 'Ruby',
+    liveBttn: '',
+    sourceBttn: '',
+  }, 
+
+  projectNro5: {
+    img: '',
+    title: 'Profesional Art Printing Data',
+    descrip: 'A daily selection of privately personalized read',
+    tech1: 'HTML',
+    tech2: 'Bootstrap',
+    tech3: 'Ruby',
+    liveBttn: '',
+    sourceBttn: '',
+  }, 
+
+  projectNro6: {
+    img: '',
+    title: 'Profesional Art Printing Data',
+    descrip: 'A daily selection of privately personalized read',
+    tech1: 'HTML',
+    tech2: 'Bootstrap',
+    tech3: 'Ruby',
+    liveBttn: '',
+    sourceBttn: '',
+  }, 
+}
+
+const projectBttn = document.querySelectorAll('.button_project'); 
+
+projectBttn.addEventListener('click', () => {
+  const popUp = document.createElement('div');
+  popUp.className('popUp-container');
+  popUp.innerHTML = `
+  
+`;
+
+const xBttn = document.getElementById('xBttn');
+})
+
+
+
