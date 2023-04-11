3752313f25fc2ebc0ea573913d2bba3c1edc5595
enmanuelbayen <enmanuel0495@gmail.com>
2023-04-10 21:23:49 -0400

    Fix JS linter error

--- JS/DOM.js
+++ JS/DOM.js
@@ -1,23 +1,19 @@
-console.log("***** Elementos del DOM.js *****");
-
 const mobileMenu = document.querySelector('.contenedor');
 const menuBttn = document.getElementById('menu-icon');
 const anchorList = document.querySelectorAll('.anchor-section');
 
 menuBttn.addEventListener('click', () => {
-    mobileMenu.style.display = 'block';
-})
+  mobileMenu.style.display = 'block';
+});
 
 const remove = document.querySelector('.x-remove');
 
 remove.addEventListener('click', () => {
-    mobileMenu.style.display = 'none';
-})
-
-anchorList.forEach((anchorList) => {
-    anchorList.addEventListener('click', () => {
-        mobileMenu.style.display = 'none';
-    });
+  mobileMenu.style.display = 'none';
 });
 
-
+anchorList.forEach((anchorList) => {
+  anchorList.addEventListener('click', () => {
+    mobileMenu.style.display = 'none';
+  });
+});
