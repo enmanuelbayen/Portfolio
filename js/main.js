8bc8e11653e30d8aade6874bb5b19822d78c0d1b
enmanuelbayen <enmanuel0495@gmail.com>
2023-04-11 15:12:31 -0400

    First model for popup

--- js/main.js
+++ js/main.js
@@ -22,82 +22,93 @@ anchorList.forEach((anchorList) => {
 const projectDeck = {
   
   projectNro1: {
-    img: '',
-    title: 'Profesional Art Printing Data',
+    img: '../img/popUp-img-P.png',
+    title: 'Profesional  project number 1',
     descrip: 'A daily selection of privately personalized read',
-    tech1: 'HTML',
-    tech2: 'Bootstrap',
-    tech3: 'Ruby',
-    liveBttn: '',
-    sourceBttn: '',
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    liveBttn: 'https://enmanuelbayen.github.io/Portfolio/',
+    sourceBttn: 'https://github.com/enmanuelbayen/Portfolio',
   }, 
 
   projectNro2: {
-    img: '',
-    title: 'Profesional Art Printing Data',
+    img: 'popUp-img-P.png',
+    title: 'Profesional  project number 2',
     descrip: 'A daily selection of privately personalized read',
-    tech1: 'HTML',
-    tech2: 'Bootstrap',
-    tech3: 'Ruby',
-    liveBttn: '',
-    sourceBttn: '',
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    liveBttn: 'https://enmanuelbayen.github.io/Portfolio/',
+    sourceBttn: 'https://github.com/enmanuelbayen/Portfolio',
   }, 
 
   projectNro3: {
-    img: '',
-    title: 'Profesional Art Printing Data',
+    img: 'popUp-img-P.png',
+    title: 'Profesional  project number 3',
     descrip: 'A daily selection of privately personalized read',
-    tech1: 'HTML',
-    tech2: 'Bootstrap',
-    tech3: 'Ruby',
-    liveBttn: '',
-    sourceBttn: '',
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    liveBttn: 'https://enmanuelbayen.github.io/Portfolio/',
+    sourceBttn: 'https://github.com/enmanuelbayen/Portfolio',
   }, 
 
   projectNro4: {
-    img: '',
-    title: 'Profesional Art Printing Data',
+    img: 'popUp-img-P.png',
+    title: 'Profesional  project number 4',
     descrip: 'A daily selection of privately personalized read',
-    tech1: 'HTML',
-    tech2: 'Bootstrap',
-    tech3: 'Ruby',
-    liveBttn: '',
-    sourceBttn: '',
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    liveBttn: 'https://enmanuelbayen.github.io/Portfolio/',
+    sourceBttn: 'https://github.com/enmanuelbayen/Portfolio',
   }, 
 
   projectNro5: {
-    img: '',
-    title: 'Profesional Art Printing Data',
+    img: 'popUp-img-P.png',
+    title: 'Profesional  project number 5',
     descrip: 'A daily selection of privately personalized read',
-    tech1: 'HTML',
-    tech2: 'Bootstrap',
-    tech3: 'Ruby',
-    liveBttn: '',
-    sourceBttn: '',
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    liveBttn: 'https://enmanuelbayen.github.io/Portfolio/',
+    sourceBttn: 'https://github.com/enmanuelbayen/Portfolio',
   }, 
 
   projectNro6: {
-    img: '',
-    title: 'Profesional Art Printing Data',
+    img: 'popUp-img-P.png',
+    title: 'Profesional  project number 6',
     descrip: 'A daily selection of privately personalized read',
-    tech1: 'HTML',
-    tech2: 'Bootstrap',
-    tech3: 'Ruby',
-    liveBttn: '',
-    sourceBttn: '',
+    tech: ['HTML', 'Bootstrap', 'Ruby'],
+    liveBttn: 'https://enmanuelbayen.github.io/Portfolio/',
+    sourceBttn: 'https://github.com/enmanuelbayen/Portfolio',
   }, 
 }
 
-const projectBttn = document.querySelectorAll('.button_project'); 
+const projectBttn = document.getElementById('project-card1'); 
 
 projectBttn.addEventListener('click', () => {
   const popUp = document.createElement('div');
   popUp.className('popUp-container');
   popUp.innerHTML = `
-  
+  <div class="project-popup-card">
+  <div class="header-popup-card">
+  <h2 class="subtitle-pop-card">${projectDeck.projectNro1.title}</h2>
+  <button class="xBttn">X</button>
+ </div> 
+ <ul class="tech-popup-card">
+  <li class="tech-list">${projectDeck.projectNro1.tech[0]}</li>
+  <li class="tech-list">${projectDeck.projectNro1.tech[1]}</li>
+  <li class="tech-list">${projectDeck.projectNro1.tech[2]}</li>
+</ul>
+ <div class="body-popup-card">
+  <img src="${projectDeck.projectNro1.img}" alt="PopUp project img">
+  <div class="text-popup-card">
+    <p class="paragrah-popup">${projectDeck.projectNro1.descrip}</p>
+    <button class="liveVersionBttn"><a href="${projectDeck.projectNro1.liveBttn}">See live</a></button>
+    <button class="sourceBttn"><a href="${projectDeck.projectNro1.sourceBttn}">See source</a></button>
+  </div>
+ </div>
+</div>
 `;
 
-const xBttn = document.getElementById('xBttn');
+body.appendchild(popUp);
+
+const close = document.querySelectorAll('xBttn');
+close.addEventListener('click', () => {
+  body.removechild(popUp);
+})
 })
 
 
